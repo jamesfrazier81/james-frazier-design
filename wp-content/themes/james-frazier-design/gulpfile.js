a8dbe9e95ca2dce0b93dc9e024b03b241cee0ee6
James Frazier <jfrazier@psstudios.com>
2019-09-24 10:12:01 -0700

    Untracked files. Newly tracked files.

--- wp-content/themes/james-frazier-design/gulpfile.js
+++ wp-content/themes/james-frazier-design/gulpfile.js
@@ -5,18 +5,18 @@ var jshint = require('gulp-jshint');
 var imagemin = require('gulp-imagemin');
 var notify = require('gulp-notify');
 var plumber = require('gulp-plumber');
-var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var browserSync = require('browser-sync').create();
 
 // Event handler message - the variable has been added to all tasks to show an error in any one via the Terminal
-var plumberErrorHandler = { errorHandler: notify.onError({
-    title: 'Gulp',
-    message: 'Error: <%= error.message %>'
-  })
+var plumberErrorHandler = {
+	errorHandler: notify.onError({
+		title: 'Gulp',
+		message: 'Error: <%= error.message %>'
+	})
 };
 
-gulp.task('serve', function() {
+gulp.task('serve', function () {
 
 	var files = [
 		'./*.php',
@@ -26,44 +26,38 @@ gulp.task('serve', function() {
 	]
 
 	browserSync.init(files, {
-		proxy: 'http://jamesfrazierdesign.test'
+		proxy: 'http://test.jamesfrazierdesign.com'
 	});
 
 	gulp.watch('./sass/*.scss', ['sass']);
 	gulp.watch(files).on('change', browserSync.reload);
 });
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
 	gulp.src('./sass/*.scss')
 		.pipe(plumber(plumberErrorHandler))
 		.pipe(sass())
 		.pipe(autoprefixer({
-            browsers: ['last 2 versions', '> 5%', 'Firefox <= 20'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('./'))
-        .pipe(browserSync.stream())
-		.pipe(notify( { message: 'TASK: "sass" 👍', onLast: true } ));
+			browsers: ['last 2 versions', '> 5%', 'Firefox <= 20'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('./'))
+		.pipe(browserSync.stream())
+		.pipe(notify({ message: 'TASK: "sass" 👍', onLast: true }));
 });
 
-gulp.task('js', function() {
-	gulp.src('./js/*.js', { base: process.cwd() })
+gulp.task('js', function () {
+	gulp.src('./js/*.js')
 		.pipe(plumber(plumberErrorHandler))
 		.pipe(jshint())
-		.pipe(uglify().on('error', function(e){
-            console.log(e);
-         }))
-		.pipe(rename({
-			dirname: '',
-			basename: 'scripts',
-            suffix: '.min',
-            extname: '.js'
-        }))
+		.pipe(uglify().on('error', function (e) {
+			console.log(e);
+		}))
 		.pipe(gulp.dest('./js/dist'))
-		.pipe(notify( { message: 'TASK: "js" 👍', onLast: true } ));
+		.pipe(notify({ message: 'TASK: "js" 👍', onLast: true }));
 });
 
-gulp.task('img', function() {
+gulp.task('img', function () {
 	gulp.src('./img/*.{png,jpg,gif}')
 		.pipe(plumber(plumberErrorHandler))
 		.pipe(imagemin({
@@ -72,10 +66,10 @@ gulp.task('img', function() {
 		}))
 
 		.pipe(gulp.dest('./img/dist'))
-		.pipe(notify( { message: 'TASK: "img" 👍', onLast: true } ));
+		.pipe(notify({ message: 'TASK: "img" 👍', onLast: true }));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
 	gulp.watch('./sass/*.scss', ['sass']);
 	gulp.watch('./js/*.js', ['js']);
 	gulp.watch('./img/*.{png,jpg,gif}', ['img']);
